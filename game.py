01a6c824876b2b5b3d38b5815a35f8b70463d1f6
nkapur25 <78129752+kapurneil@users.noreply.github.com>
2022-06-20 16:04:40 -0400

    add drop_piece method

--- game.py
+++ game.py
@@ -3,11 +3,22 @@ class Game:
         self.board = [[0 for i in range(7)] for i in range(6)]
 
     def __repr__(self):
-        str_board = "" #creates string that will be returned
-        for i in self.board:
+        str_board = "" #creates string of board that will be returned
+        for row in self.board:
             str_board += "\n" #each nested list represents a row so a new line is started when a list is iterated through
-            for j in i:
-                if j == 0: str_board += ". " #0 represents blank space
-                if j == 1: str_board += "R " #1 represents red
-                if j == 2: str_board += "Y " #2 represents yellow
+            for element in row:
+                if element == 0: str_board += ". " #0 represents blank space
+                if element == 1: str_board += "R " #1 represents red
+                if element == 2: str_board += "Y " #2 represents yellow
         return str_board
+
+    def drop_piece(self, player, column):
+        #checks to see if a piece can be dropped in a column.
+        #if column is full, -1 is returned, otherwise the row is returned and the board is updated
+        row = -1
+        for check_row in range(5, -1, -1):
+            if self.board[check_row][int(column)] == 0:
+                row = check_row
+                self.board[row][int(column)] = player #if piece can be dropped, board is updated
+                break
+        return row #returns row if piece is dropped or -1 if column is full
